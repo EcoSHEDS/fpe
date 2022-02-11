0a7030a10b27595416b062c7065e6704f0265edd
Jeff Walker <jeff@walkerenvres.com>
2022-02-11 02:40:58 +0000

    add export-stations.R

---
+++ r/src/export-stations.R
@@ -0,0 +1,85 @@
+# export data and images for a given station
+# usage: Rscript export-station.R <id> <output dir>
+
+Sys.setenv(TZ = "GMT")
+
+library(tidyverse)
+library(lubridate)
+library(logger)
+
+args <- commandArgs(trailingOnly = TRUE)
+station_id <- parse_number(args[1])
+output_dir <- args[2]
+
+log_info("station_id: {station_id}")
+log_info("output_dir: {output_dir}")
+
+stopifnot(dir.exists(output_dir))
+
+config <- config::get()
+
+con <- DBI::dbConnect(
+  RPostgreSQL::PostgreSQL(),
+  host = config$db$host,
+  port = config$db$port,
+  dbname = config$db$database,
+  user = config$db$user,
+  password = config$db$password
+)
+
+fetch_station <- function(con, station_id) {
+  DBI::dbGetQuery(con, "select * from stations where id = $1", list(station_id)) %>%
+    as_tibble()
+}
+
+fetch_station_values <- function(con, station_id) {
+  DBI::dbGetQuery(con, "
+select s.dataset_id, s.id as series_id, s.variable_id, v.timestamp, v.value, v.flag from
+datasets d
+left join series s on d.id = s.dataset_id
+left join values v on s.id = v.series_id
+where d.station_id = $1
+", list(station_id)) %>%
+    as_tibble()
+}
+
+fetch_station_images <- function(con, station_id) {
+  DBI::dbGetQuery(con, "
+select i.imageset_id, i.id as image_id, i.timestamp, i.filename, i.full_url as url from
+imagesets iset
+left join images i on iset.id = i.imageset_id
+where iset.station_id = $1
+and i.status='DONE'
+", list(station_id)) %>%
+    as_tibble()
+}
+
+log_info("fetching: station (id={station_id})")
+station <- fetch_station(con, station_id)
+stopifnot(nrow(station) == 1)
+
+log_info("fetching: values (id={station_id})")
+values <- fetch_station_values(con, station_id)
+
+log_info("fetching: images (id={station_id})")
+images <- fetch_station_images(con, station_id)
+
+station_dir <- file.path(output_dir, station$name)
+if (!dir.exists(station_dir)) {
+  log_info("creating: {station_dir}")
+  dir.create(station_dir, showWarnings = FALSE, recursive = TRUE)
+}
+
+if (nrow(values) > 0) {
+  log_info("saving: {file.path(station_dir, 'values.csv')} (n={scales::comma(nrow(values))})")
+  write_csv(values, file.path(station_dir, "values.csv"), na = "")
+} else {
+  log_warn("no values")
+}
+
+if (nrow(images) > 0) {
+  log_info("saving: {file.path(station_dir, 'images.csv')} (n={scales::comma(nrow(images))})")
+  write_csv(images, file.path(station_dir, "images.csv"), na = "")
+} else {
+  log_warn("no images")
+}
