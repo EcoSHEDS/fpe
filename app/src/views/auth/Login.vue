338089911d75c23c13c2e8e7c35faf5315bde1fd
Jeff Walker <jdwalker@contractor.usgs.gov>
2022-12-22 20:18:29 -0500

    fix login delay

--- app/src/views/auth/Login.vue
+++ app/src/views/auth/Login.vue
@@ -153,8 +153,6 @@ export default {
           default:
             this.error = this.$errorMessage(err)
         }
-        return
-      } finally {
         this.loading = false
       }
     },
