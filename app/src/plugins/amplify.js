86e8f730b5dd835558f2052ece1dfd2b15c72dcc
Jeff Walker <jeff@walkerenvres.com>
2023-01-14 21:23:14 -0500

    update amplify

--- app/src/plugins/amplify.js
+++ app/src/plugins/amplify.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import Amplify, * as AmplifyModules from 'aws-amplify'
+import { Amplify } from 'aws-amplify'
+import * as AmplifyModules from 'aws-amplify'
 import { AmplifyPlugin } from 'aws-amplify-vue'
 
 Amplify.configure({
