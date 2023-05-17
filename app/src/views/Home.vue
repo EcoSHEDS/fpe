22fdf31b8ad89804b80ba0f2283bbac68f46170d
Jeff Walker <jdwalker@contractor.usgs.gov>
2023-05-17 16:08:37 -0400

    app: disclaimer

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -8,47 +8,54 @@
       </v-col>
     </v-row>
     <v-row justify="space-around">
-      <v-col cols="12" lg="6" xl="5" class="body-1 black--text" :class="{ 'px-8': $vuetify.breakpoint.lg }">
-        <v-alert
-          type="warning"
-          border="left"
-          text
-          colored-border
-          class="body-2"
-          v-if="$vuetify.breakpoint.mobile"
-        >
-          <div class="font-weight-bold body-1">Not Optimized for Mobile Devices</div>
-          <div>This website is primarily designed for latop or desktop computers.</div>
-        </v-alert>
+      <v-col cols="12" lg="6" xl="6" class="body-1 black--text" :class="{ 'px-8': $vuetify.breakpoint.lgAndUp }">
+        <div class="mx-4">
+          <v-alert
+            type="warning"
+            border="left"
+            text
+            colored-border
+            class="body-2"
+            v-if="$vuetify.breakpoint.mobile"
+          >
+            <div class="font-weight-bold body-1">Not Optimized for Mobile Devices</div>
+            <div>This website is primarily designed for latop or desktop computers.</div>
+          </v-alert>
 
-        <p class="">
-          The <strong>Flow Photo Explorer</strong> is an <strong>integrated database, machine learning, and data visualization</strong> platform for monitoring <strong>streamflow</strong> and other hydrologic conditions using <strong>timelapse images</strong>. The goal of this project is to develop new approaches to monitoring hydrologic conditions in streams and rivers where flow data are historically sparse or non-existent.
-        </p>
+          <p>
+            The <strong>Flow Photo Explorer</strong> is an integrated database, machine learning, and data visualization platform for monitoring streamflow and other hydrologic conditions using timelapse images.
+          </p>
+          <p>
+            The goal of this project is to develop new approaches to hydrologic monitoring in streams and rivers where flow data are historically sparse or non-existent.
+          </p>
 
-        <v-img src="img/fpe-diagram.png" alt="FPE diagram of images to machine learning models to estimated streamflow" class="mx-4 my-8"></v-img>
+          <v-img src="img/fpe-diagram.png" alt="FPE diagram of images to machine learning models to estimated streamflow" class="mx-4 my-8"></v-img>
 
-        <div class="text-center my-10">
-          <v-btn color="success" x-large :to="{ name: 'explorer' }">Start Exploring <v-icon>mdi-chevron-right</v-icon></v-btn>
+          <div class="text-center my-10">
+            <v-btn color="success" x-large :to="{ name: 'explorer' }">Start Exploring <v-icon>mdi-chevron-right</v-icon></v-btn>
+          </div>
+
+          <p>
+            <strong>Do you have flow photos to contribute?</strong> <router-link :to="{ name: 'request' }">Request an account</router-link> to upload your photos.<br>
+            <strong>Already have an account?</strong> <router-link :to="{ name: 'login' }">Log in</router-link>.<br>
+            <strong>Questions?</strong> You can reach us at <a href="mailto:ecosheds@usgs.gov">ecosheds@usgs.gov</a>.
+          </p>
         </div>
-
-        <p>
-          <strong>Do you have flow photos to contribute?</strong> <router-link :to="{ name: 'request' }">Request an account</router-link> to upload your photos.<br>
-          <strong>Already have an account?</strong> <router-link :to="{ name: 'login' }">Log in</router-link>.<br>
-          <strong>Questions?</strong> You can reach us at <a href="mailto:ecosheds@usgs.gov">ecosheds@usgs.gov</a>.
-        </p>
       </v-col>
-      <v-col cols="12" lg="6" xl="5" class="body-1 black--text" :class="{ 'px-8': $vuetify.breakpoint.lg }">
+      <v-col cols="12" lg="6" xl="6" class="body-1 black--text" :class="{ 'px-8': $vuetify.breakpoint.lgAndUp }">
         <video src="video/AIML_FINALDRAFT_v3_optim.mp4" controls width="100%" poster="video/AIML_FINALDRAFT_v3_optim.jpg"></video>
 
-        <p class="font-weight-bold text--secondary font-italic">Video produced by the <a href="https://www.usgs.gov/centers/md-de-dc-water">USGS MD-DE-DC Water Science Center</a></p>
+        <div class="mx-4">
+          <p class="font-weight-bold text--secondary font-italic">Video produced by the <a href="https://www.usgs.gov/centers/md-de-dc-water">USGS MD-DE-DC Water Science Center</a></p>
 
-        <p>
-          The Flow Photo Explorer project is a collaboration between U.S. Geological Survey, U.S. Environmental Protection Agency, Walker Environmental Research, Microsoft Research, and many contributing partners. Funding was provided by U.S. Geological Survey, U.S. Environmental Protection Agency, and National Geographic Society. See <router-link :to="{ name: 'about' }">About</router-link> for more information.
-        </p>
+          <p>
+            The Flow Photo Explorer project is a collaboration between U.S. Geological Survey, U.S. Environmental Protection Agency, Walker Environmental Research, Microsoft Research, and many contributing partners. Funding was provided by U.S. Geological Survey, U.S. Environmental Protection Agency, and National Geographic Society. See <router-link :to="{ name: 'about' }">About</router-link> for more information.
+          </p>
+        </div>
       </v-col>
     </v-row>
     <v-row justify="space-around">
-      <v-col cols="12" lg="6" xl="5" class="body-1 black--text" :class="{ 'px-8': $vuetify.breakpoint.lg }">
+      <v-col cols="12" lg="6" xl="6" class="body-1 black--text" :class="{ 'px-8': $vuetify.breakpoint.lgAndUp }">
         <v-toolbar flat dense color="grey lighten-3 mb-4">
           <v-toolbar-title>
             <h2 class="text-h5">Deep Learning Model</h2>
@@ -73,8 +80,19 @@
           </p>
           <div class="body-2"><a href="mailto:ecosheds@usgs.gov">Contact us</a> for a copy of this publication.</div>
         </div>
+
+        <v-toolbar flat dense color="grey lighten-3 mt-8 mb-4">
+          <v-toolbar-title>
+            <h2 class="text-h5">Disclaimer</h2>
+          </v-toolbar-title>
+        </v-toolbar>
+        <Alert type="error" title="Provisional Database" class="mb-0 mx-4">
+          <div style="font-family:monospace">
+            The data you have secured from the U.S. Geological Survey (USGS) database identified as the Flow Photo Explorer (FPE) have not received USGS approval and as such are provisional and subject to revision. The data are released on the condition that neither the USGS nor the U.S. Government shall be held liable for any damages resulting from its authorized or unauthorized use.
+          </div>
+        </Alert>
       </v-col>
-      <v-col cols="12" lg="6" xl="5" class="body-1 black--text">
+      <v-col cols="12" lg="6" xl="6" class="body-1 black--text">
         <v-toolbar flat dense color="grey lighten-3 mb-4">
           <v-toolbar-title>
             <h2 class="text-h5">What's New?</h2>
@@ -91,7 +109,7 @@
           <div class="body-1">
             <p>All photos are now screened for <strong>Personal Identifying Information (PII)</strong> using <a href="https://github.com/ecologize/CameraTraps/blob/main/megadetector.md">MegaDetector</a> to detect people and vehicles. Images with suspected PII are <u>not shown</u> on the <i>Photo Explorer</i>, but can be reviewed by station owners in the <i>Upload</i> section.</p>
             <p><strong>Stations</strong> can now be categorized by <strong>Waterbody Type</strong> (e.g., stream, lake, etc.) and <strong>Status</strong> (active vs. discontinued). By default, all stations have been marked as "Stream" and "Active". <strong>Please review your stations</strong> in the <i>Upload</i> section to confirm.</p>
-            <p>We have a great <strong>new video</strong> about why and how we created FPE, and what it means to us. We have also added detailed <strong>instructions for uploading photos</strong> to FPE in the <router-link :to="{ name: 'user-guide' }">User Guide</router-link>. And lastly, the homepage has received some much-needed <strong>sprucing up</strong>!</p>
+            <p>We have a great <strong>new video</strong> about why and how we created FPE, and what it means to us. We have also added detailed <strong>instructions for uploading photos</strong> to FPE in the <router-link :to="{ name: 'user-guide' }">User Guide</router-link>. And lastly, the homepage has received some much-needed sprucing up!</p>
           </div>
         </div>
 
@@ -104,11 +122,11 @@
         <div class="mx-4">
           <p><span class="font-weight-bold"><u>Phase I (2020-2022)</u></span>: a database and cloud-based data pipeline was developed for storing, managing, and accessing timelapse imagery of streams and rivers along with associated flow and stage data. The system allows registered users to upload and manage their own photos and (optionally) flow data at multiple locations. The images and flow data are accessible through the <router-link :to="{ name: 'explorer' }">Photo Explorer</router-link>, which provides an interactive and exploratory interface for viewing the timelapse imagery coupled with observed flow data. The images and flow data in the FPE database will serve as the primary data source for developing and training the machine learning models in Phase II.</p>
 
-          <p><span class="font-weight-bold"><u>Phase II (ongoing)</u></span>: the photos and data uploaded to FPE will be used to develop <strong>deep learning models</strong> for estimating <strong>flow</strong> and other hydrologic conditions using <strong>timelapse imagery</strong>. Two types of models are currently being developed (see <a href="https://doi.org/10.1145/3530190.3534805">Gupta et al., 2022</a> for details):</p>
+          <p><span class="font-weight-bold"><u>Phase II (ongoing)</u></span>: the photos and data uploaded to FPE will be used to develop <strong>deep learning models</strong> for estimating flow and other hydrologic conditions using timelapse imagery. Two types of models are currently being developed (see <a href="https://doi.org/10.1145/3530190.3534805">Gupta et al., 2022</a> for details):</p>
 
           <ol class="my-4">
-            <li>The first model is a classic <strong>regression model</strong> trained using both images and observed (or estimated) flows at each site. This model is designed to estimate the <strong>flow rate</strong> of each image directly. However, because it <strong>requires observed flow data for training</strong>, this model will primarily be used <strong>for comparison</strong> to our second model, which requires only images.</li>
-            <li>The second model is a <strong>ranking model</strong> trained using human annotations of <strong>pairwise image comparisons</strong> whereby a person is repeatedly asked which of two images shows more flow. From these pairwise image annotations, the model learns how to <strong>sort the images</strong> from lowest to highest flow. The rank (or percentile) of each image then provides a <strong>relative measure of streamflow</strong>, which alone can provide valuable information such as the duration of droughts or timing of peak flows. The relative streamflow can then be used to estimate <strong>volumetric flow rates</strong> by passing those percentiles through an assumed or estimated statistical distribution of streamflow at each site. The <strong>critical benefit</strong> of the image ranking model is that it <strong>does not require any observed flow or stage data for training, only images</strong>.</li>
+            <li>The first model is a classic <strong>regression model</strong> trained using both images and observed (or estimated) flows at each site. This model is designed to estimate the flow rate of each image directly. However, because it requires observed flow data for training, this model will primarily be used for comparison to our second model, which requires only images.</li>
+            <li>The second model is a <strong>ranking model</strong> trained using human annotations of pairwise image comparisons whereby a person is repeatedly asked which of two images shows more flow. From these pairwise image annotations, the model learns how to sort the images from lowest to highest flow. The rank (or percentile) of each image then provides a relative measure of streamflow, which alone can provide valuable information such as the duration of droughts or timing of peak flows. The relative streamflow can then be used to estimate volumetric flow rates by passing those percentiles through an assumed or estimated statistical distribution of streamflow at each site. The critical benefit of the image ranking model is that it does not require any observed flow or stage data for training, only images.</li>
           </ol>
 
           <p>In addition, other types of models including detecting flow/no flow, or the presence of ice are also being explored. Lastly, all images are now screened for the presence of <strong>Personal Identifying Information (PII)</strong> using the <a href="https://github.com/ecologize/CameraTraps/blob/main/megadetector.md">MegaDetector</a> object detection model.</p>
