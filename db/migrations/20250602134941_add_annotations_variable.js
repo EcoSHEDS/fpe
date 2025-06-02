33b486e4b449b89b312715827aa2200166c072c6
Jeff Walker <jdwalker@contractor.usgs.gov>
2025-06-02 11:09:04 -0400

    refactor(db): annotations.variable default

--- db/migrations/20250602134941_add_annotations_variable.js
+++ db/migrations/20250602134941_add_annotations_variable.js
@@ -1,6 +1,6 @@
 
 exports.up = knex => knex.schema.table('annotations', t => {
-  t.text('variable').notNullable()
+  t.text('variable').default('FLOW').notNullable()
 })
 
 exports.down = knex => knex.schema.alterTable('annotations', t => {
