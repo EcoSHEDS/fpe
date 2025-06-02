7df3f1f8185e76cd9a48eab75bfa4324d4b214d5
Jeff Walker <jeff@walkerenvres.com>
2025-06-02 10:49:32 -0400

    refactor(db): change variable FK in annotations to text
    Variables table is really designed for datasets, not model variables. Remove annotations.variable_id (FK) and add annotations.variable (text) instead.

--- db/migrations/20250602134941_add_annotations_variable.js
+++ db/migrations/20250602134941_add_annotations_variable.js
@@ -1,11 +1,8 @@
 
 exports.up = knex => knex.schema.table('annotations', t => {
-  t.string('variable_id', 16)
-    .references('variables.id')
-    .index()
-    .onDelete('SET NULL')
+  t.text('variable').notNullable()
 })
 
 exports.down = knex => knex.schema.alterTable('annotations', t => {
-  t.dropColumn('variable_id')
+  t.dropColumn('variable')
 })
